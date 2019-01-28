6cfd4e2bbd87674e55ba03633edee02ba22eff6d
Sajid Jahabar Ali <46625151+sajidjahabarali@users.noreply.github.com>
2019-01-28 17:57:34 +0000

    starting machine learning net worth application project with hello world test

---
+++ main.py
@@ -0,0 +1,10 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+#import numpy as np
+#import matplotlib.pyplot as plt
+
+print(tf.__version__)
+print('hello world')
