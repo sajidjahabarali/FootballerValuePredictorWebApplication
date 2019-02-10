a9aabfbec08ccb8a55c8ba1c46ba1175340d42e2
Sajid Jahabar Ali <46625151+sajidjahabarali@users.noreply.github.com>
2019-02-10 14:10:20 +0000

    imported a fifa 19 dataset to get the stats of different players and printed the stats.

--- main.py
+++ main.py
@@ -1,10 +1,103 @@
-# TensorFlow and tf.keras
+from __future__ import absolute_import, division, print_function
+import pathlib
+import pandas as pd
+import seaborn as sns
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow.keras import layers
+import matplotlib.pyplot as plt
+# store directory containing data in a variable
+DATAPATH = 'C:\\Users\\sajid\\OneDrive\\Documents\\University\\Final Year Project\\Data\\fifa19\\data.csv'
 
-# Helper libraries
-#import numpy as np
-#import matplotlib.pyplot as plt
-#hello world
-print(tf.__version__)
-print('hello world')
+# import the dataset using pandas
+columnNames = ['Name',
+               'Age',
+               'Nationality',
+               'Overall',
+               'Potential',
+               'Club',
+               'Value(€M)',
+               'Wage(€K)',
+               'Special',
+               'PreferredFoot',
+               'InternationalReputation',
+               'WeakFoot',
+               'SkillMoves',
+               'WorkRate',
+               'BodyType',
+               'Position',
+               'JerseyNumber',
+               'Height(cm)',
+               'Weight(lbs)',
+               'LS',
+               'ST',
+               'RS',
+               'LW',
+               'LF',
+               'CF',
+               'RF',
+               'RW',
+               'LAM',
+               'CAM',
+               'RAM',
+               'LM',
+               'LCM',
+               'CM',
+               'RCM',
+               'RM',
+               'LWB',
+               'LDM',
+               'CDM',
+               'RDM',
+               'RWB',
+               'LB',
+               'LCB',
+               'CB',
+               'RCB',
+               'RB',
+               'Crossing',
+               'Finishing',
+               'HeadingAccuracy',
+               'ShortPassing',
+               'Volleys',
+               'Dribbling',
+               'Curve',
+               'FKAccuracy',
+               'LongPassing',
+               'BallControl',
+               'Acceleration',
+               'SprintSpeed',
+               'Agility',
+               'Reactions',
+               'Balance',
+               'ShotPower',
+               'Jumping',
+               'Stamina',
+               'Strength',
+               'LongShots',
+               'Aggression',
+               'Interceptions',
+               'Positioning',
+               'Vision',
+               'Penalties',
+               'Composure',
+               'Marking',
+               'StandingTackle',
+               'SlidingTackle',
+               'GKDiving',
+               'GKHandling',
+               'GKKicking',
+               'GKPositioning',
+               'GKReflexes',
+               'ReleaseClause(€M)'
+               ]
+
+raw_dataset = pd.read_csv(DATAPATH, names=columnNames, na_values="?", comment='\t', sep=",", skipinitialspace=True, encoding='latin-1')
+#pd.set_option('display.max_columns', 70)
+# make a copy of the dataset to leave the original unaffected
+dataset = raw_dataset.copy()
+# dataset = dataset.dropna();
+# printing the dataset
+print(dataset.tail())
+
+
